0d5e6004e8c5d72b1634265d1a549b03d9f9e3b9
katzy687 <katzy687@gmail.com>
2022-08-01 21:14:31 +0300

    add all

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const fs = require('fs')
+require('dotenv').config()
 
 const app = express()
 
@@ -8,10 +10,20 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.get('*', (req, res) => {
-  res.send('OK')
-})
+app.set('view engine', 'ejs');
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('App listening on port ' + listener.address().port)
+// FOR STATIC FILES (css,js etc.)
+app.use(express.static(__dirname + '/public'));
+
+// home route
+app.get('/', (req, res) => {
+  let data = {
+    foo: 'FOO',
+    dbstring: process.env.DB_CONNECTION_STRING
+  }
+  res.render('index', data);
+});
+
+const listener = app.listen(process.env.PORT || 3001, () => {
+  console.log('App listening on port:  ' + listener.address().port)
 })
